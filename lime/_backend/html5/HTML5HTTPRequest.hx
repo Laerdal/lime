fc77f50c4056013a624b34010a79cf4630213f7b
Greg Caldwell <caldwellgp@googlemail.com>
2017-01-09 14:14:36 +0000

    Increase the XMLHttpRequest timeouts from 30 to 90 seconds as some files are large.

--- lime/_backend/html5/HTML5HTTPRequest.hx
+++ lime/_backend/html5/HTML5HTTPRequest.hx
@@ -59,6 +59,7 @@ class HTML5HTTPRequest {
 	private function load (uri:String, progress:Dynamic, readyStateChange:Dynamic):Void {
 		
 		request = new XMLHttpRequest ();
+		request.timeout = 90000;
 		request.addEventListener ("progress", progress, false);
 		request.onreadystatechange = readyStateChange;
 		
