628855dbc1d1b7b0d08bca944a0590744afda64b
Greg Caldwell <caldwellgp@googlemail.com>
2017-01-11 10:15:34 +0000

    Added new <window httpReqTimeout="" > attribute to project.xml to allow overriding the default 30000 ms HttpRequest timeout

--- lime/project/ProjectXMLParser.hx
+++ lime/project/ProjectXMLParser.hx
@@ -2125,7 +2125,7 @@ class ProjectXMLParser extends HXProject {
 						
 					}
 				
-				case "height", "width", "fps", "antialiasing":
+				case "height", "width", "fps", "antialiasing", "httpReqTimeout":
 					
 					if (Reflect.hasField (windows[id], name)) {
 						
